9e4a477bfd45d95922e70801a7d1815f784b5c58
aishlohiya <aishlohiya@github.com>
2021-09-29 16:28:50 +0530

    update: final version

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quiz-form");
 const quizButton = document.querySelector("#quiz-button");
 const quizOutput = document.querySelector("#quiz-output")
 
-const quizAnswers = ["90°","Right angled" ]
+const quizAnswers = ["90°","Right angled","one right angle","12, 16, 20","Equilateral triangle","100°","30°","a + b + c","no","45°", ]
 
 function quizQuestions() {
     let score = 0;
